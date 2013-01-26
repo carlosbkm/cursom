f42019ca8358854f36543e3a66ac593bddce25ce
Cristian Sepulveda <air.enrill@gmail.com>
2013-01-25 21:57:01 -0300

    Cambio para las categorias
    cambio en el formulario para que se actualicen las categorias
    automaticamente

--- cursom/apps/productos/forms.py
+++ cursom/apps/productos/forms.py
@@ -2,9 +2,7 @@
 from django import forms
 from cursom.apps.productos.models import Categoria
 
-choicess = ((categoria.id, categoria.nombre) for categoria in Categoria.objects.all())
-
 class AgregarForm(forms.Form):
 	nombre = forms.CharField(widget = forms.TextInput(), required=True)
-	categoria = forms.ChoiceField(choices=choicess, label="Categoría", required=True)
+	categoria = forms.ModelChoiceField(Categoria.objects.all(), required=True)
 	descripcion = forms.CharField(widget = forms.Textarea(), label="Descripción", required=True)
